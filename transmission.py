2ea90fa1a5ca8cce8ae0df6bb775f77ef9d25d5d
abarrientosrivas <alexisbarrientos97@gmail.com>
2023-09-13 13:27:24 -0300

    transmission finished

--- transmission.py
+++ transmission.py
@@ -1,7 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, Field
 from typing import List
-from aio_pika.exceptions import ProbableAuthenticationError, AMQPConnectionError, ChannelNotFoundEntity
+from enum import Enum
+from aio_pika.exceptions import ProbableAuthenticationError, AMQPConnectionError, ChannelNotFoundEntity, ChannelClosed
+from aiormq.exceptions import ChannelAccessRefused
+from aio_pika import Connection
 import tomli, asyncio, sys, aio_pika
+
+class ExchangeType(Enum):
+    DIRECT = "direct"
+    FANOUT = "fanout"
+    TOPIC = "topic"
 
 class MessageBind(BaseModel):
     exchange_name: str
@@ -9,19 +17,20 @@ class MessageBind(BaseModel):
 
 class MessageExchange(BaseModel):
     name: str
-    type: str
-    durable: bool
+    type: ExchangeType = ExchangeType.FANOUT
+    durable: bool = False
 
 class MessageQueue(BaseModel):
     name: str
-    binds: List[MessageBind]
+    durable: bool = False
+    binds: List[MessageBind] = Field(default_factory=list)
 
 class TransmissionConfig(BaseModel):
     host: str
     user: str
     password: str
-    exchanges: List[MessageExchange]
-    queues: List[MessageQueue]
+    exchanges: List[MessageExchange] = Field(default_factory=list)
+    queues: List[MessageQueue] = Field(default_factory=list)
 
 def exit_on_error(message:str):
     print(message, file=sys.stderr)
@@ -29,47 +38,73 @@ def exit_on_error(message:str):
 
 async def main(config: TransmissionConfig):
     try:
-        connection = await aio_pika.connect(
-            f"amqp://{config.user}:{config.password}@{config.host}/"
-        )
-    except ProbableAuthenticationError:
-        exit_on_error("Login to RabbitMQ failed: please check your username and password")
-    except AMQPConnectionError:
-        exit_on_error("Connection failed: unable to connect to the RabbitMQ server")
-    except Exception as e:
-        exit_on_error(f"An unexpected error occurred while connecting to RabbitMQ server: {e}")
-
-    channel = await connection.channel()
-
-    for exchange_data in config.exchanges:
         try:
-            await channel.declare_exchange(exchange_data.name, passive=True)
-            print(f"Exchange with name {exchange_data.name} already exists.", file=sys.stderr)
-        except ChannelNotFoundEntity as e:
-            channel = await connection.channel()
-            await channel.declare_exchange(exchange_data.name,type=exchange_data.type,durable=exchange_data.durable,passive=False)
-            print(f"Exchange with name {exchange_data.name} declared.", file=sys.stderr)
+            connection = await aio_pika.connect(
+                f"amqp://{config.user}:{config.password}@{config.host}/"
+            )
+        except ProbableAuthenticationError:
+            exit_on_error("Login to RabbitMQ failed: please check your username and password")
+        except AMQPConnectionError:
+            exit_on_error("Connection failed: unable to connect to the RabbitMQ server")
+        except Exception as e:
+            exit_on_error(f"An unexpected error occurred while connecting to RabbitMQ server: {e}")
 
-    for queue_data in config.queues:
+        for exchange_data in config.exchanges:
+            await set_up_exchange(connection,exchange_data)
+
+        for queue_data in config.queues:
+            await set_up_queue(connection,queue_data)
+
+            for bind_data in queue_data.binds:
+                await set_up_bind(connection, queue_data.name, bind_data)
+                
+        print("Finished executing.", file=sys.stderr)
+    except asyncio.CancelledError:
+        print("Execution was cancelled prematurely.", file=sys.stderr)
+
+async def set_up_exchange(connection: Connection, exchange_data: MessageExchange):
+    try:
+        channel = await connection.channel()
+        try:
+            exchange = await channel.declare_exchange(exchange_data.name, passive=True)
+            print(f"Exchange with name '{exchange.name}' already exists.", file=sys.stderr)
+        except ChannelNotFoundEntity:
+            channel = await connection.channel()
+            exchange = await channel.declare_exchange(exchange_data.name,type=exchange_data.type.value,durable=exchange_data.durable,passive=False)
+            print(f"Exchange with name '{exchange.name}' declared.", file=sys.stderr)
+    except ChannelAccessRefused:
+        print(f"Channel has refused access on exchange '{exchange_data.name}'. Check you login configuration and user permission.", file=sys.stderr)
+
+async def set_up_queue(connection: Connection, queue_data: MessageQueue):
+    try:
+        channel = await connection.channel()
         try:
             queue = await channel.declare_queue(queue_data.name, passive=True)
-            print(f"Queue with name {queue.name} already exists.", file=sys.stderr)
-        except ChannelNotFoundEntity as e:
+            print(f"Queue with name '{queue.name}' already exists.", file=sys.stderr)
+        except ChannelNotFoundEntity:
             channel = await connection.channel()
-            queue = await channel.declare_queue(queue_data.name, passive=False)
-            print(f"Queue with name {queue.name} declared.", file=sys.stderr)
+            queue = await channel.declare_queue(queue_data.name, durable=queue_data.durable, passive=False)
+            print(f"Queue with name '{queue.name}' declared.", file=sys.stderr)
+    except ChannelAccessRefused:
+        print(f"Channel has refused access on queue '{queue_data.name}'. Check you login configuration and user permission.", file=sys.stderr)
 
-        for bind in queue_data.binds:
-            try:
-                exchange = await channel.get_exchange(bind.exchange_name)
-                await queue.bind(exchange,bind.routing_key)
-                print(f"Queue with name {queue_data.name} bound to exchange {bind.exchange_name} with routing key {bind.routing_key}.", file=sys.stderr)
-            except ChannelNotFoundEntity as e:
-                channel = await connection.channel()
-                print(f"Exchange with name {bind.exchange_name} does not exist. Check your configuration file.", file=sys.stderr)
-            
-    print("done")
-
+async def set_up_bind(connection: Connection, queue_name: str, bind_data: MessageBind):
+    try:
+        channel = await connection.channel()
+        try:
+            exchange = await channel.get_exchange(bind_data.exchange_name)
+        except ChannelNotFoundEntity:
+            print(f"Exchange with name '{bind_data.exchange_name}' does not exist. Check your configuration file.", file=sys.stderr)
+            return
+        try:
+            queue = await channel.get_queue(queue_name)
+        except ChannelNotFoundEntity:
+            print(f"Queue with name '{queue_name}' does not exist. Check your configuration file.", file=sys.stderr)
+            return
+        await queue.bind(exchange.name,bind_data.routing_key)
+        print(f"Queue with name '{queue_name}' bound to exchange {bind_data.exchange_name} with routing key '{bind_data.routing_key}'.", file=sys.stderr)
+    except ChannelAccessRefused:
+        print(f"Channel has refused access binding queue '{queue_name}' with exchange '{bind_data.exchange_name}'. Check you login configuration and user permission.", file=sys.stderr)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
@@ -77,9 +112,11 @@ if __name__ == "__main__":
     else:
         exit_on_error("Expected configuration file path as argument.")
     try:
-        with open(config_file_path, "rb") as f:
-            config_dict = tomli.load(f)
+        with open(config_file_path, "rb") as file:
+            config_dict = tomli.load(file)
             config = TransmissionConfig(**config_dict)
+    except ValidationError as e:
+        exit_on_error(f"Failed to validate configuration: {e}")
     except tomli.TOMLDecodeError as e:
         exit_on_error(f"Failed to parse TOML file: {e}")
     except FileNotFoundError:
