7c088111efc54abcf9ab16b5ae543f1071dae9ee
Alexis Barrientos Rivas <alexisbarrientos97@gmail.com>
2023-09-28 07:13:25 -0300

    writting document memories process

--- document_memories.py
+++ document_memories.py
@@ -7,30 +7,45 @@ from aio_pika import Connection
 from espa import exit_on_error
 import tomli, asyncio, sys, aio_pika
 
-class ExchangeType(Enum):
-    DIRECT = "direct"
-    FANOUT = "fanout"
-    TOPIC = "topic"
-
-class MessageBind(BaseModel):
-    exchange_name: str
-    routing_key: str
-
-class MessageExchange(BaseModel):
-    name: str
-    type: ExchangeType = ExchangeType.FANOUT
-    durable: bool = False
-
-class MessageQueue(BaseModel):
-    name: str
-    durable: bool = False
-    binds: List[MessageBind] = Field(default_factory=list)
+class DatabaseType(int):
+    IN_MEMORY = 1
+    ON_FILE = 2
 
 class DocumentMemoriesConfig(BaseModel):
     host: str
     user: str
     password: str
     assimilate_file_mq: str
+    memories_exchange: str
+    memories_routing_key: str
+    database_type: DatabaseType
+
+async def main(config: DocumentMemoriesConfig):
+    try:
+        try:
+            connection = await aio_pika.connect(
+                f"amqp://{config.user}:{config.password}@{config.host}/"
+            )
+        except ProbableAuthenticationError:
+            exit_on_error("Login to RabbitMQ failed: please check your username and password")
+        except AMQPConnectionError:
+            exit_on_error("Connection failed: unable to connect to the RabbitMQ server")
+        except Exception as e:
+            exit_on_error(f"An unexpected error occurred while connecting to RabbitMQ server: {e}")
+
+        # start consuming from the right queue
+                
+        print("Finished executing.", file=sys.stderr)
+    except asyncio.CancelledError:
+        print("Execution was cancelled prematurely.", file=sys.stderr)
+
+async def consume_file():
+    # try to read the file
+    # save general metadata
+    # batch content for storage
+    # store every batch
+    # report success on exchange
+    pass
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
